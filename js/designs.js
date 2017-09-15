3009f90484b57bfa9d3857b90a8ed883e3ffa316
leonwindrunner <leeleonfly@gmail.com>
2017-09-14 19:00:22 -0700

    some change

--- js/designs.js
+++ js/designs.js
@@ -1,10 +1,34 @@
-// Select color input
-// Select size input
+function makeGrid() {	
+	var canvasWidth=Number($("#input_width").val());
+	var canvasHeight=Number($("#input_height").val());
+	var canvasTable=$("#pixel_canvas");
+	var boxColor;
 
-// When size is submitted by the user, call makeGrid()
+	function createTable() {
+		for(let r=0;r<canvasWidth;r++) {
+			let row=$('<tr id="row-'+r+'"></tr>');
+			canvasTable.append(row);
+			for(let c=0;c<canvasHeight;c++) {
+				let column=$("<td></td>");
+				$("#row-"+r).append(column);
+			}
+		}
+	}
 
-function makeGrid() {
+	createTable();
 
-// Your code goes here!
+	$("#pixel_canvas").on("click", "td", function() {
+		boxColor=$("#colorPicker").val();
+		$(this).css("backgroundColor",boxColor);
+	});
 
+	$("#submitBtn").click(function(e) {
+		e.preventDefault();
+		canvasWidth=Number($("#input_width").val());
+		canvasHeight=Number($("#input_height").val());
+		canvasTable.empty();
+		createTable();
+	});	
 }
+
+$(document).ready(makeGrid());
