0ad2c470408bd359924ee9a94a67982a7e6ff41f
leonwindrunner <leeleonfly@gmail.com>
2017-09-22 18:07:57 -0700

    js format

--- js/designs.js
+++ js/designs.js
@@ -1,34 +1,35 @@
-function makeGrid() {	
-	var canvasWidth=Number($("#input_width").val());
-	var canvasHeight=Number($("#input_height").val());
-	var canvasTable=$("#pixel_canvas");
-	var boxColor;
+function makeGrid() {
+  var canvasWidth = Number($("#input_width").val());
+  var canvasHeight = Number($("#input_height").val());
+  var canvasTable = $("#pixel_canvas");
+  var boxColor;
 
-	function createTable() {
-		for(let r=0;r<canvasWidth;r++) {
-			let row=$('<tr id="row-'+r+'"></tr>');
-			canvasTable.append(row);
-			for(let c=0;c<canvasHeight;c++) {
-				let column=$("<td></td>");
-				$("#row-"+r).append(column);
-			}
-		}
-	}
+  function createTable() {
+    for (let r = 0; r < canvasHeight; r++) {
+      let row = $('<tr id="row-' + r + '"></tr>');
+      canvasTable.append(row);
+      for (let c = 0; c < canvasWidth; c++) {
+        let column = $("<td></td>");
+        $("#row-" + r).append(column);
+      }
+    }
+  }
 
-	createTable();
+  createTable();
 
-	$("#pixel_canvas").on("click", "td", function() {
-		boxColor=$("#colorPicker").val();
-		$(this).css("backgroundColor",boxColor);
-	});
+  $("#pixel_canvas").on("click", "td", function() {
+    boxColor = $("#colorPicker").val();
+    $(this).css("backgroundColor", boxColor);
+  });
 
-	$("#submitBtn").click(function(e) {
-		e.preventDefault();
-		canvasWidth=Number($("#input_width").val());
-		canvasHeight=Number($("#input_height").val());
-		canvasTable.empty();
-		createTable();
-	});	
+  $("#submitBtn").click(function(e) {
+    e.preventDefault();
+    canvasWidth = Number($("#input_width").val());
+    canvasHeight = Number($("#input_height").val());
+    canvasTable.empty();
+    createTable();
+    // 引用自己
+  });
 }
 
 $(document).ready(makeGrid());
